aeff4458dd9e1c2683bc7939a6b0eca06b911972
lix <sclx1220@163.com>
2019-03-15 17:26:30 +0800

    提交

---
+++ lix-cloud-consumer/src/main/java/com/lix/cloud/consumer/config/RedisManagerConfig.java
@@ -0,0 +1,11 @@
+package com.lix.cloud.consumer.config;
+
+/**
+ * redis声明式缓存配置
+ * @author lix
+ * @Date 2019/3/15
+ */
+public class RedisManagerConfig {
+
+
+}
--- lix-cloud-producer/src/main/java/com/lix/cloud/producer/ProducerApplication.java
+++ lix-cloud-producer/src/main/java/com/lix/cloud/producer/ProducerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 public class ProducerApplication {
 
