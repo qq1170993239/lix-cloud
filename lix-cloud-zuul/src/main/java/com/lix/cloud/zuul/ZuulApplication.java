43dcc9b7e4c09a651a8bb958de627c4d3df193e3
lix <sclx1220@163.com>
2019-03-15 18:02:51 +0800

    zuul

--- lix-cloud-zuul/src/main/java/com/lix/cloud/zuul/ZuulApplication.java
+++ lix-cloud-zuul/src/main/java/com/lix/cloud/zuul/ZuulApplication.java
@@ -2,15 +2,16 @@ package com.lix.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
-@EnableDiscoveryClient
-public class LixCloudZuulApplication {
+@SpringBootApplication
+public class ZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LixCloudZuulApplication.class, args);
+		SpringApplication.run(ZuulApplication.class, args);
 	}
 
 }
---
+++ lix-cloud-zuul/src/main/java/com/lix/cloud/zuul/filter/MyZuulFilter.java
@@ -0,0 +1,36 @@
+package com.lix.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2019/3/15 0015.
+ */
+@Component
+public class MyZuulFilter  extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
